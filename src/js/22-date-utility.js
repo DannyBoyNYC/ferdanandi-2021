5313aaf327aca4a414a2a658722af6d5ae4ea1fd
Daniel Deverell <daniel.deverell@gmail.com>
2021-07-19 10:27:03 -0400

    done

---
+++ src/js/22-date-utility.js
@@ -0,0 +1,61 @@
+let stamp = (function () {
+  const times = {
+    hours: 1000 * 60 * 60,
+    days: 1000 * 60 * 60 * 24,
+    weeks: 1000 * 60 * 60 * 24 * 7,
+    months: 1000 * 60 * 60 * 24 * 30,
+    years: 1000 * 60 * 60 * 24 * 365,
+  };
+
+  const format = {
+    dateStyle: 'long',
+    timeStyle: 'short',
+    hour12: true,
+  };
+
+  const addDays = (timestamp, n) => {
+    return timestamp + n * times['days'];
+  };
+
+  const addWeeks = (timestamp, n) => {
+    return timestamp + n * times['weeks'];
+  };
+
+  const addMonths = (timestamp, n) => {
+    return timestamp + n * times['months'];
+  };
+
+  const addYears = (timestamp, n) => {
+    return timestamp + n * times['years'];
+  };
+
+  const getDate = (timestamp) => {
+    let newDate = new Date(timestamp);
+    return newDate.toLocaleString(navigator.language, format);
+  };
+
+  return { getDate, addDays, addWeeks, addMonths, addYears };
+})();
+
+// Get a timestamp for right now
+let now = new Date().getTime();
+
+// Add three weeks
+let threeWeeks = stamp.addWeeks(now, 3);
+let dateOne = stamp.getDate(threeWeeks);
+console.log(' in three weeks ', dateOne);
+
+// Add three days
+let threeDays = stamp.addDays(now, 3);
+let dateTwo = stamp.getDate(threeDays);
+console.log(' in three days ', dateTwo);
+
+// Add three Months
+let threeMonths = stamp.addMonths(now, 3);
+let dateThree = stamp.getDate(threeMonths);
+console.log(' in three Months ', dateThree);
+
+// Add three Years
+let threeYears = stamp.addYears(now, 3);
+let dateFour = stamp.getDate(threeYears);
+console.log(' in three Years ', dateFour);
