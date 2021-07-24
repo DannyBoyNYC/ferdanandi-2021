6fd55e7bca738192828b157c8034a67465de6bb8
Daniel Deverell <daniel.deverell@gmail.com>
2021-07-24 09:06:07 -0400

    done

--- src/js/23-date-constructor.js
+++ src/js/23-date-constructor.js
@@ -11,6 +11,11 @@ let Stamp = (function () {
     this.timestamp = date ? new Date(date).getTime() : new Date().getTime();
   }
 
+  Constructor.prototype.addHours = function (n = 1) {
+    this.n = n * times.hours;
+    return this;
+  };
+
   Constructor.prototype.addDays = function (n = 1) {
     this.n = n * times.days;
     return this;
@@ -43,7 +48,23 @@ let Stamp = (function () {
 
 // Create a new Stamp() instance for right now
 let now = new Stamp();
+let fourYears = new Stamp();
+let fourUnitsFromNow = fourYears
+  .addHours(4)
+  // .addWeeks(3)
+  // .addDays(3)
+  // .addYears(3)
+  .getDate();
+console.log(' fourUnitsFromNow ', fourUnitsFromNow);
 
-// let aWhileAgo = now.addWeeks().addDays(2).addYears(-3).getDate();
 let aWhileAgo = now.addWeeks().addDays(2).addYears(-3).getDate();
 console.log(' aWhileAgo ', aWhileAgo);
+
+const testProto = Object.getPrototypeOf(now);
+console.log(' testProto ', testProto);
+
+const stampProto = Stamp.prototype;
+console.log(' stampProto ', stampProto);
+
+const isInstance = now instanceof Stamp;
+console.log(' isInstance ', isInstance);
